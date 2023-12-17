078cda2ffe4d0c27c037fd4c7238e502cd121370
Washington Irving <iamwashingyou@gmail.com>
2023-12-17 23:28:33 +0300

    add lsp keymaps

--- lua/wi/core/lsp-config.lua
+++ lua/wi/core/lsp-config.lua
@@ -1,3 +1,7 @@
+vim.keymap.set("n", "<leader>x", vim.diagnostic.open_float)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gh", vim.lsp.buf.hover)
+
 local lspconfig = require("lspconfig")
 
 lspconfig.lua_ls.setup {}
--- lua/wi/plugin-configs/treesitter.lua
+++ lua/wi/plugin-configs/treesitter.lua
@@ -1,10 +1,1 @@
-require("nvim-treesitter.configs").setup {
-    ensure_installed = {
-        "lua",
-        "typescript",
-        "rust",
-        "go",
-        "html",
-        "css",
-    },
-}
+require("nvim-treesitter.configs").setup {}
