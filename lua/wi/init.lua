eeb87163356be917d74de8e2042141668b0afda7
Washington Irving <iamwashingyou@gmail.com>
2023-12-17 22:26:00 +0300

    add keymaps for nvim-tree

--- lua/wi/init.lua
+++ lua/wi/init.lua
@@ -2,3 +2,5 @@ require("wi.core.options")
 require("wi.core.remap")
 require("wi.core.plugins")
 require("wi.core.colorscheme")
+
+require("wi.plugin-configs.nvim-tree")
---
+++ lua/wi/plugin-configs/nvim-tree.lua
@@ -0,0 +1,23 @@
+local function on_attach(bufnr)
+    local api = require "nvim-tree.api"
+
+    local function opts(desc)
+        return {
+            desc = "nvim-tree: " .. desc,
+            noremap = true,
+            silent = true,
+            nowait = true,
+        }
+    end
+
+    api.config.mappings.default_on_attach(bufnr)
+
+    vim.keymap.set("n", "<leader>E", api.tree.toggle, opts("Toggle"))
+    vim.keymap.set("n", "<leader>e", api.tree.focus, opts("Focus"))
+    vim.keymap.set("n", "<leader><TAB>", api.tree.find_file, opts("Find file"))
+end
+
+-- pass to setup along with your other options
+require("nvim-tree").setup {
+    on_attach = on_attach,
+}
